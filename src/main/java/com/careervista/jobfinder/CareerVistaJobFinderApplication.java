f8ebb79e8713349c21351adcf45e16e4cfb64843
iamchandrask <chandrashekar.kalmood@gmail.com>
2025-10-24 16:03:09 +0530

    [chandrashekar] feat: add initial project structure to dev branch

---
+++ src/main/java/com/careervista/jobfinder/CareerVistaJobFinderApplication.java
@@ -0,0 +1,13 @@
+package com.careervista.jobfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerVistaJobFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerVistaJobFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careervista/jobfinder/CareerVistaJobFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careervista.jobfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerVistaJobFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
